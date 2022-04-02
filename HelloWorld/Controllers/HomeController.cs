914e11c1ca3aa0fffe120fe849a1069ff3a4f600
Max Nadvornyi <ipzk201_nmyu@student.ztu.edu.ua>
2022-04-02 22:09:28 +0300

    Hello Name

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using HelloWorld.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+            
+
+        [HttpGet]
+        public ViewResult HelloWorld()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult HelloWorld(Name name)
+        {
+            if (ModelState.IsValid)
+                return View("HelloName", name);
+            else
+                return View();
+        }
+
+        
+       
+    }
+}
---
+++ HelloWorld/Models/Name.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelloWorld.Models
+{
+    public class Name
+    {
+        [Required(ErrorMessage = "Enter your name")]
+        [RegularExpression("^[a-zA-Zа-яА-Я]+$", ErrorMessage = "You entered the wrong value")]
+        public string NamePerson { get; set; }
+    }
+}
